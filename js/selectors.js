4825e9b189ec63fdbb4cb094ee13265ae9271d12
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-26 17:20:15 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,11 +1,3 @@
-$("img")
-$(".pics")
-$("#baby-ninja")
-$('ul li')
-$('div img:first-child')
-$("img[alt='the beatles making faces']")
-$('div:last')
-
 function paragraphSelector(){
   return $("p");
 }
